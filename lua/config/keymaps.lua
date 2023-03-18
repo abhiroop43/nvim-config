2ffc3fc8d4f94716fbd2fd88b356b14917734b60
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-03-18 18:42:13 +0400

    added lazygit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree focus<cr>")
+
+-- setup mapping to call :LazyGit
+vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>")
